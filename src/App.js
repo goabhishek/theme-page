10b37dad71b8caa5e6e90f788eef797a8b9e1d38
Abhishek Mishra <mabhishek4156@gmai.com>
2022-01-20 16:41:35 +0530

    update git

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useLayoutEffect } from 'react';
 import './App.css';
 
 function App() {
@@ -6,26 +6,36 @@ function App() {
   const getMode = () => {
     return JSON.parse(localStorage.getItem("mode")) || false
   }
-  const [dark, setMode] = useState(getMode());
+  const [dark, setMode] = useState(false);
+
+  useLayoutEffect(() => {
+    if (localStorage.getItem("mode")) {
+      setMode(JSON.parse(localStorage.getItem("mode")))
+    }
+
+  }, [])
+
+
+  console.log({ dark })
+
 
   useEffect(() => {
+
     localStorage.setItem("mode", JSON.stringify(dark))
   }, [dark])
 
   return (
-    <div className={dark ? "daek-mode" : "App"}>
+    <div className={`${dark ? "dark-theme" : "light-theme"}-App`}>
       <div className="nav">
         <label className="switch">
-          <input type="checkbox" cheked={dark} onChange={() => setMode(!dark)} />
+          <input type="checkbox" checked={dark} onChange={() => setMode(dark => !dark)} />
           <span className="slider round"></span>
         </label>
       </div>
       <div className='content'>
         <h1>{dark ? "Dark mode is on" : "Light Mode is On"}</h1>
-        <p></p>
+
       </div>
-
-
     </div>
   );
 }
